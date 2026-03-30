1281273432ef19cbb418d311597aac91b8247483
Marcos jordao <mstjsousa@gmail.com>
2026-03-29 22:59:55 -0300

    impedir que o banco se apague novamente

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\App;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,5 +23,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Vite::prefetch(concurrency: 3);
+        if (App::environment('production')) {
+            DB::preventStagingMigration();
+        }
     }
 }
